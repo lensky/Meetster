7c5ded6f3f528c167ea17e5a5ccc687499b86f7b
Michael <Michael@18.111.106.80>
2013-04-06 19:23:15 -0400

    starting settings page

--- Meetster/src/com/russia/meetster/MainActivity.java
+++ Meetster/src/com/russia/meetster/MainActivity.java
@@ -22,12 +22,6 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	/** Called when the user clicks the Send button */
-	public void sendMessage(View view) {
-		Intent intent = new Intent(this, CreateEventActivity.class);
-		EditText editText = (EditText) findViewById(R.id.editText1);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-	    // Do something in response to button
-	}
+
 
 }
