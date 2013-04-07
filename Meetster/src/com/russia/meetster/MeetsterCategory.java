8a5e9f89610f39292dc2177dd3f4e83b2bcc232c
Yuri D. Lensky <ydl@mit.edu>
2013-04-06 20:26:21 -0400

    Added toValues() to MeetsterCategory.

--- Meetster/src/com/russia/meetster/MeetsterCategory.java
+++ Meetster/src/com/russia/meetster/MeetsterCategory.java
@@ -1,24 +1,41 @@
 package com.russia.meetster;
 
+import android.content.ContentValues;
+
 public class MeetsterCategory {
-	private long id;
+	private Long id;
 	private String description;
+
 	
-	public MeetsterCategory(long id, String description) {
-		this.id = id;
-		this.description = description;
+	public MeetsterCategory(String description) {
+		this.setDescription(description);
+		this.setId(null);
 	}
 	
 	public long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	
+	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public boolean hasId() {
+		return (id != null);
+	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	public ContentValues toValues() {
+		ContentValues vals = new ContentValues();
+		vals.put(MeetsterContract.Categories.DESCRIPTION, this.getDescription());
+		
+		return vals;
 	}
 }
